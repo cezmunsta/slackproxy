6b93d3db3a0060851680cc61e94e61c180f5849b
cezmunsta <ceri.williams@percona.com>
2021-07-15 13:31:13 +0100

    Added test for SlackProxy.channel_rules
    * Refactored slackproxy.test.test_app.SLACKPROXY_SERVER_ARGS to
      SERVER_ARGS and merge it with the DEFAULT_CONFIG from
      inheritence
    * Added slackproxy.test.test_app.AppTestCase.DEFAULT_CONFIG
      as a copy of SERVER_ARGS
    * Refactored slackproxy.test.test_app to use srv instead of _app
    * Added slackproxy.test.test_app.AppTestCase.test_channel_rules
      to verify that channel rules work as expected when empty

--- lib/slackproxy/test/test_app.py
+++ lib/slackproxy/test/test_app.py
@@ -7,33 +7,48 @@ import unittest
 from libsrv.test.test_service import ServiceTestCase
 from slackproxy import app
 
-SLACKPROXY_SERVER_ARGS = {
-    'db_name': 'test.db',
-    'debug': True,
-}
+SERVER_ARGS = ServiceTestCase.DEFAULT_CONFIG.copy()
+SERVER_ARGS.update({'db_name': 'test.db', 'debug': True, })
 
 
 class AppTestCase(ServiceTestCase):
     """
     Test case for SlackProxy
     """
-    _database = SLACKPROXY_SERVER_ARGS['db_name']
+    DEFAULT_CONFIG = SERVER_ARGS.copy()
+
+    _database = SERVER_ARGS['db_name']
 
     def setUp(self) -> None:
         """
+        Shared configuration and preparation tasks
 
         :return:
         """
         super().setUp()
-        self._app = app.SlackProxy(**SLACKPROXY_SERVER_ARGS)
+        self.srv = app.SlackProxy(**SERVER_ARGS)
 
     def test_run(self) -> None:
         """
+        Test the generation of a deferred reactor
 
         :return:
         """
-        self.srv = self._app
         super().test_run()
+
+    def test_channel_rules(self) -> None:
+        """
+        Test handling of rules
+
+        :return:
+        """
+        self.assertEqual(len(self.srv.channel_rules), 0)
+        with self.srv.db as dbc:
+            try:
+                dbc.execute('REPLACE INTO channel_rules VALUES(?, ?, 1)', ('dummy', 'dummy'))
+            except sqlite3.OperationalError:
+                pass
+        self.assertNotEqual(len(self.srv.channel_rules), 0)
 
 
 if __name__ == '__main__':
